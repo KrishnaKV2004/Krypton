19a810d27dc092da7f67fbdd98e4090c91e0ddac
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2025-04-12 15:15:13 +0530

    Make Structure

---
+++ inc/lexer.hpp
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------
+
+                            Lexical Analyzer Header File
+
+---------------------------------------------------------------------------------------*/
+
+#ifndef LEXER_HPP
+#define LEXER_HPP
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#endif // LEXER_HPP
